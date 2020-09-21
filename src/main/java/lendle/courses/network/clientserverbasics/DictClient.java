33a6c925cc133409635cfc25b6587cc36fe9c45c
user <user@70802-PC25>
2020-09-21 16:57:29 +0800

    suprise mother fuxker

--- src/main/java/lendle/courses/network/clientserverbasics/DictClient.java
+++ src/main/java/lendle/courses/network/clientserverbasics/DictClient.java
@@ -25,24 +25,15 @@ public class DictClient {
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
         Socket socket = new Socket("dict.org", 2628);
-        socket.setSoTimeout(15000);
-        //hint: 從 socket 取得 OutputStream
-        OutputStream out = null;
-        ////////////////////////////////////////////
-        Writer writer = new OutputStreamWriter(out, "UTF-8");
-        //hint: 輸出 DEFINE wn gold\r\n
-        writer.write("");
-        /////////////////////////////
+        OutputStream out = socket.getOutputStream();
+        Writer writer = new OutputStreamWriter(out,"UTF-8");
+        writer.write("DEFINE wn gold\r\n");
         writer.flush();
-        //hint: 從socket 取得 InputStream
-        InputStream in = null;
-        ////////////////////////////////
+        InputStream in = socket.getInputStream();
         BufferedReader reader = new BufferedReader(
-                new InputStreamReader(in, "UTF-8"));
-        for (String line = reader.readLine(); !line.equals("."); line = reader.readLine()) {
-            //hint: 將 line 變數輸出到終端機
-            System.out.println();
-            //////////////////////
+            new InputStreamReader(in,"UTF-8"));
+        for(String line = reader.readLine(); !line.equals("."); line=reader.readLine()){
+            System.out.print(line);
         }
         writer.write("quit\r\n");
         writer.flush();
